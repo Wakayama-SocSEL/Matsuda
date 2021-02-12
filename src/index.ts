716d6a99f747bf5d1386ac95ceea6c9adcca5f83
mzdkzk <mzdkzk@yahoo.co.jp>
2021-02-12 20:47:56 +0900

    更新

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import git from "isomorphic-git";
 import http from "isomorphic-git/http/node";
 
 type DiffResult = {
-  filepath: string;
-  type: "modify" | "add" | "remove" | "equal";
+  filename: string;
+  type: "modify" | "add" | "remove";
 };
 
 //https://isomorphic-git.org/docs/en/snippets#git-diff-name-status-commithash1-commithash2
@@ -15,14 +15,18 @@ async function diff(
   hash1: string,
   hash2: string
 ): Promise<DiffResult[]> {
-  const diffs: DiffResult[] = await git.walk({
+  return await git.walk({
     fs,
     dir,
     trees: [git.TREE({ ref: hash1 }), git.TREE({ ref: hash2 })],
-    // @ts-ignore
-    map: async (filepath, [A, B]): DiffResult => {
-      const Aoid = await A.oid();
-      const Boid = await B.oid();
+    map: async (filename, entries): Promise<DiffResult | undefined> => {
+      if (entries == null) {
+        return undefined;
+      }
+      const [A, B] = entries;
+      // 型定義が誤っており、A,Bはnullになることがある
+      const Aoid = A && (await A.oid());
+      const Boid = B && (await B.oid());
       const type =
         Aoid !== Boid
           ? "modify"
@@ -31,15 +35,40 @@ async function diff(
           : Boid === undefined
           ? "remove"
           : "equal";
-      return { filepath, type };
+      if (type == "equal") {
+        return undefined;
+      }
+      return { filename, type };
     },
   });
-  return diffs.filter((diff) => diff.filepath != ".");
+}
+
+type Data = {
+  url: string;
+  containsTest: boolean;
+  diff: DiffResult[];
+};
+
+async function getData(
+  dir: string,
+  url: string,
+  tags: string[]
+): Promise<Data[]> {
+  const promises = [...new Array(tags.length - 1)].map(async (_, i) => {
+    const diffs = await diff(dir, tags[i], tags[i + 1]);
+    return {
+      url: `${url}/compare/${tags[i]}...${tags[i + 1]}`,
+      diff: diffs,
+      containsTest:
+        diffs.find((diff) => /test/.test(diff.filename)) !== undefined,
+    };
+  });
+  return await Promise.all(promises);
 }
 
 const main = async () => {
   const urls = [
-    //"https://github.com/expressjs/express",
+    "https://github.com/expressjs/express",
     "https://github.com/npm/node-semver",
   ];
   for (const url of urls) {
@@ -48,8 +77,12 @@ const main = async () => {
     if (!fs.existsSync(dir)) {
       await git.clone({ fs, http, dir, url: `${url}.git` });
     }
-    const diffs = await diff(dir, "v7.3.3", "v7.3.4");
-    console.log(diffs.filter((diff) => diff.type != "equal"));
+    const tags = await git.listTags({ fs, dir });
+    const result = await getData(dir, url, tags);
+    fs.writeFileSync(
+      `./output/${repo}.json`,
+      JSON.stringify({ result }, null, 2)
+    );
   }
 };
 
