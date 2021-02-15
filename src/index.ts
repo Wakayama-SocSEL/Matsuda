62ef650e8655f0dd7a180c88e4175147410fcea6
mzdkzk <mzdkzk@yahoo.co.jp>
2021-02-15 22:57:25 +0900

    修正

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,16 @@ import childProcess from "child_process";
 import git from "isomorphic-git";
 import http from "isomorphic-git/http/node";
 
-type GitDiffResult = {
+type GitDiff = {
   change: string;
   filename: string;
 };
 
-function diff(
-  dir: string,
-  hash1: string,
-  hash2: string
-): Promise<GitDiffResult[]> {
-  return new Promise<GitDiffResult[]>((resolve) => {
+function diff(dir: string, hash1: string, hash2: string): Promise<GitDiff[]> {
+  return new Promise<GitDiff[]>((resolve) => {
     const command = `cd ${dir} && git diff --raw ${hash1} ${hash2}`;
     const diffBuffer = childProcess.execSync(command);
-    const diffs = diffBuffer.toString("utf-8").split("\n");
+    const diffs = diffBuffer.toString("utf-8").trim().split("\n");
     const result = diffs.map((diff) => {
       const [change, filename] = diff.split(/\s/).slice(-2);
       return { change, filename };
@@ -28,19 +24,24 @@ function diff(
 }
 
 type Data = {
-  url: string;
+  compareUrl: string;
   containsTest: boolean;
-  diff: GitDiffResult[];
+  diffs: GitDiff[];
 };
 
 function getData(dir: string, url: string, tags: string[]): Promise<Data[]> {
   const promises = [...new Array(tags.length - 1)].map(async (_, i) => {
     const diffs = await diff(dir, tags[i], tags[i + 1]);
+    const compareUrl = `${url}/compare/${tags[i]}...${tags[i + 1]}`;
+    const containsTest =
+      diffs.find((diff) => {
+        return /test/.test(diff.filename) && diff.change;
+      }) !== undefined;
+    console.log(compareUrl, containsTest);
     return {
-      url: `${url}/compare/${tags[i]}...${tags[i + 1]}`,
-      diff: diffs,
-      containsTest:
-        diffs.find((diff) => /test/.test(diff.filename)) !== undefined,
+      compareUrl,
+      diffs,
+      containsTest,
     };
   });
   return Promise.all(promises);
