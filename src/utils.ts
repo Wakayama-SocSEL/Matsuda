d80ae4a323f1a02833ebee290d03b5430eebc12c
mzdkzk <mzdkzk@yahoo.co.jp>
2021-03-21 20:00:24 +0900

    修正

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,7 @@ export async function parallelPromiseAll<T>(
   const results: T[] = [];
   let cursor = 0;
   let completed = 0;
-  showProgress(cursor, tasks.length);
+  showProgress(completed, tasks.length);
   const processes = Array.from({ length: concurrency }).map(async () => {
     while (true) {
       const index = cursor++;
