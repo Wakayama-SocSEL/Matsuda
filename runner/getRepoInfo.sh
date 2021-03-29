cff9731027994f59bcbb31d3e4aae604508b7cfa
mzdkzk <mzdkzk@yahoo.co.jp>
2021-03-29 16:53:53 +0900

    jqのパースエラーを無視するように

--- runner/getRepoInfo.sh
+++ runner/getRepoInfo.sh
@@ -2,4 +2,7 @@
 
 cd repos/$1 &&\
   git log --reverse --date-order --pretty="%H" |\
-  xargs -Ihash bash -c "git show hash:package.json 2>/dev/null | jq '.version' | xargs -Iversion echo version hash"
+  xargs -Ihash bash -c \
+    "git show hash:package.json 2>/dev/null |
+     jq '.version' 2>/dev/null |
+     xargs -Iversion echo version hash"
