84736a8058a63c243a58872d80e09dff26ea1b85
mzdkzk <mzdkzk@yahoo.co.jp>
2021-03-29 16:41:34 +0900

    実験データをnpms_scoreに基づいて制限

--- runner/getInput.sh
+++ runner/getInput.sh
@@ -4,5 +4,8 @@ jq -s -R 'split("\n") |
   map(split(",")) |
   .[1:-1] |
   map(select(.[25] == "1")) |
+  sort_by(.[-1]) |
+  reverse |
+  .[:30] |
   { repoNames: map(.[7]) }'\
   ../dataset/pkg_repositories.csv > input.json
