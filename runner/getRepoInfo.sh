30c7f27a7bdc5a6588901f93cd915533e5ab47fd
mzdkzk <mzdkzk@yahoo.co.jp>
2021-04-09 15:15:43 +0900

    .versionがnullの時は何も返さないように

--- runner/getRepoInfo.sh
+++ runner/getRepoInfo.sh
@@ -4,5 +4,5 @@ cd repos/$1 &&\
   git log --reverse --date-order --pretty="%H" |\
   xargs -Ihash bash -c \
     "git show hash:package.json 2>/dev/null |
-     jq '.version' 2>/dev/null |
+     jq 'if .version == null then empty else .version end' 2>/dev/null |
      xargs -Iversion echo version hash"
