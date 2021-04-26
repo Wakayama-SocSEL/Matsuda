bdf67f5845b7289e6e2614b3331e4d199cc49e6a
mzdkzk <mzdkzk@yahoo.co.jp>
2021-04-26 21:42:53 +0900

    pkg_repositories.csvの型追加

--- src/types.ts
+++ src/types.ts
@@ -21,3 +21,51 @@ type PromiseType<T extends Promise<any>> = T extends Promise<infer P>
 export type RepoStatus = {
   [version: string]: PromiseType<ReturnType<Octokit["request"]>>;
 };
+
+// pkg_repositories.csv
+type DatasetRepository = {
+  npm_pkg: string;
+  language_name: string;
+  releases_count: number;
+  issues_count: number;
+  pullRequests_count: number;
+  stargazers_count: number;
+  watchers_count: number;
+  nameWithOwner: string;
+  pushedAt: string;
+  updatedAt: string;
+  createdAt: string;
+  isFork: string;
+  forkCount: number;
+  isArchived: string;
+  isMirror: string;
+  isLocked: string;
+  id: string;
+  databaseId: number;
+  parent_nameWithOwner: string;
+  commits_count: number;
+  commits_dependencies_count: number;
+  contributors_count: number;
+  readmeSize: number;
+  testsSize: number;
+  license: string;
+  hasTestScript: string;
+  hasSelectiveFiles: string;
+  npm_starsCount: number;
+  carefulness: number;
+  tests: number;
+  health: number;
+  branding: number;
+  communityInterest: number;
+  downloadsCount: number;
+  downloadsAcceleration: number;
+  dependentsCount: number;
+  releasesFrequency: number;
+  commitsFrequency: number;
+  openIssues: number;
+  issuesDistribution: number;
+  quality: number;
+  popularity: number;
+  maintenance: number;
+  npms_score: number;
+};
