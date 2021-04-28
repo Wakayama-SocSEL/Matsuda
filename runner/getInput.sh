1df1478792ae7f509f35c511f1710a0221bfdb8a
mzdkzk <mzdkzk@yahoo.co.jp>
2021-04-28 15:39:35 +0900

    input.json更新

--- runner/getInput.sh
+++ runner/getInput.sh
@@ -7,7 +7,12 @@ if [ ! -e ../dataset/dataset.db ]; then
 fi
 
 QUERY="
-SELECT * FROM pkg_repositories
+SELECT
+  npm_pkg,
+  nameWithOwner,
+  hasTestScript,
+  npms_score
+FROM pkg_repositories
   WHERE hasTestScript=1
   ORDER BY npms_score DESC
 "
