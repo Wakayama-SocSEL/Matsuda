56b2cbf3857fe2e61c892888e167b4160bebfad9
mzdkzk <mzdkzk@yahoo.co.jp>
2021-05-02 00:27:17 +0900

    更新

--- runner/getRepoInfo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd repos/$1 &&\
-  git log --reverse --date-order --pretty="%H" |\
-  xargs -Ihash bash -c \
-    "git show hash:package.json 2>/dev/null |
-     jq 'if .version == null then empty else .version end' 2>/dev/null |
-     xargs -Iversion echo version hash"
--- runner/runTest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd repos/$1 &&\
-  git reset $2 --hard &&\
-  (npm ci || npm install) &&\
-  npm test
