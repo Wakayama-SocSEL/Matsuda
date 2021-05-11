9e35d986876d1a61618ab8f24a3f14b2a7811646
mzdkzk <mzdkzk@yahoo.co.jp>
2021-05-11 19:32:51 +0900

    sqlにインデックス追加

--- scripts/initDB.sh
+++ scripts/initDB.sh
@@ -4,4 +4,5 @@ if [ ! -e ./dataset/dataset.db ]; then
   ls -1 ./dataset/*.csv |\
     sed 's/\.\/dataset\/\(.*\)\.csv/\1/g' |\
     xargs -i sqlite3 -csv ./dataset/dataset.db ".import ./dataset/{}.csv {}"
+  sqlite3 ./dataset/dataset.db < ./scripts/sql/createIndex.sql
 fi
---
+++ scripts/sql/createIndex.sql
@@ -0,0 +1,2 @@
+CREATE INDEX nameWithOwner_idx ON pkg_repositories(nameWithOwner);
+CREATE INDEX dependency_idx ON pkg_dependencies(dependency);
