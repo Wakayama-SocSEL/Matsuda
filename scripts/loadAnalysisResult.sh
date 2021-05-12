5bdafacf0e76f8ba07fee3942d55d042ea0f7234
mzdkzk <mzdkzk@yahoo.co.jp>
2021-05-12 19:17:21 +0900

    修正

--- scripts/loadAnalysisResult.sh
+++ scripts/loadAnalysisResult.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-ls -1 ./output/*.csv |\
-  sed 's/\.\/output\/\(.*\)\.csv/\1/g' |\
-  xargs -i sqlite3 -csv ./dataset/dataset.db ".import ./output/{}.csv {}"
+sqlite3 -csv ./dataset/dataset.db ".import ./output/repository_versions.csv repository_versions"
+sqlite3 -csv ./dataset/dataset.db ".import ./output/analysis_result.csv analysis_result"
