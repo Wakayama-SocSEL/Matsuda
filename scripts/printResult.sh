cb3ee36253ad380b7136dba6f64781bdd6a43412
mzdkzk <mzdkzk@yahoo.co.jp>
2021-06-23 14:19:44 +0900

    更新

--- scripts/printResult.sh
+++ scripts/printResult.sh
@@ -1,7 +1,18 @@
-SB=`jq 'map(select(.state=="success" and .isBreaking)) | length' ./output/proposal_result.json`
-FB=`jq 'map(select(.state=="failure" and .isBreaking)) | length' ./output/proposal_result.json`
-SnB=`jq 'map(select(.state=="success" and (.isBreaking | not))) | length' ./output/proposal_result.json`
-FnB=`jq 'map(select(.state=="failure" and (.isBreaking | not))) | length' ./output/proposal_result.json`
-echo "$FB\t$FnB"
-echo "$SB\t$SnB"
-echo "再現率: "`node -e "console.log($FB / ($FB + $FnB))"`
+function aggregate() {
+  B=`jq 'map(select(.state=="'$1'" and .isBreaking)) | length' ./output/proposal_result.json`
+  nB=`jq 'map(select(.state=="'$1'" and (.isBreaking | not))) | length' ./output/proposal_result.json`
+  echo "$B\t$nB"
+  echo "再現率: "`node -e "console.log($B / ($B + $nB))"`
+}
+aggregate failure
+aggregate success
+
+function successRange() {
+  SB=`jq 'map(select(.state=="success")) | sort_by(.stats.success) | .['$1':'$2'] | map(select(.isBreaking)) | length' ./output/proposal_result.json`
+  SnB=`jq 'map(select(.state=="success")) | sort_by(.stats.success) | .['$1':'$2'] | map(select(.isBreaking | not)) | length' ./output/proposal_result.json`
+  echo "$SB\t$SnB\t"`node -e "console.log($SnB / ($SB + $SnB))"`
+}
+successRange 0 120
+successRange 120 240
+successRange 240 360
+successRange 360
