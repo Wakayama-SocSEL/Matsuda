6c15bd92cdd8b4049e1b5e992d7fdbd9936ba103
mzdkzk <mzdkzk@yahoo.co.jp>
2021-12-15 10:27:29 +0900

    修正

--- scripts/printResult.sh
+++ scripts/printResult.sh
@@ -17,7 +17,7 @@ const f = `aggregate2 failure 0`
 const s = `aggregate2 success 20`
 console.log(',テスト変更あり<br>(後方互換性なしの可能性が高い),テスト変更なし<br>(後方互換性なしの可能性が低い)')
 console.log('ソフトウェアが一つ以上失敗<br>(実際の後方互換性なし),' + f.join(','))
-console.log('ソフトウェアが全て以上失敗<br>(実際の後方互換性不明),' + s.join(','))
+console.log('ソフトウェアが全て以上成功<br>(実際の後方互換性不明),' + s.join(','))
 console.log('再現率,' + f[0] / (f[0] + f[1]))
 console.log('適合率,' + f[0] / (f[0] + s[0]))
 " | node | nkf -s > result.csv
--- utils/printError.sh
+++
@@ -1,8 +0,0 @@
-jq -r '
-map(select(
-  .L__hash=="'$1'"
-)) 
-| map(
-  .S__nameWithOwner + "\n" + .err + "\n====================="
-) 
-| .[]' ./output/test_result.json | less
